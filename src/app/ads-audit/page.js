6dc9f135492b8da4cfdf4c5a1e0ecb3e338f3ce0
Xpression-web <roshan.singh@xpressioncorp.com>
2025-06-07 15:30:52 +0530

    cta banner and form in ads audit

--- src/app/ads-audit/page.js
+++ src/app/ads-audit/page.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import Navbar from '../asample/naviagtion'
-import CookieConsent from '../cookies/cookie'
-import AdsAudit from './home'
-import PPCAuditSection from './what'
-import ServicesAccordion from './include'
-import TestimonialSection from './coatation'
-import PPCAudit from './happens'
-import InsightSection from '../graphics/insight'
-import Footer from '../(mainpage)/footer'
+import React from "react";
+import CTASection from "./ctabanner";
+import ContactForm from "./form";
+import Navbar from "../asample/naviagtion";
+import CookieConsent from "../cookies/cookie";
+import AdsAudit from "./home";
+import PPCAuditSection from "./what";
+import ServicesAccordion from "./include";
+import TestimonialSection from "./coatation";
+import PPCAudit from "./happens";
+import InsightSection from "../graphics/insight";
+import Footer from "../(mainpage)/footer";
 
 const page = () => {
   return (
     <div>
-     <Navbar/>
-    <CookieConsent/>
-    <AdsAudit/>
-    <PPCAuditSection/>
-    <ServicesAccordion/>
-    <TestimonialSection/>
-    <PPCAudit/>
-    <InsightSection/>
-    <Footer/>
+      <Navbar />
+      <CookieConsent />
+      <AdsAudit />
+      <PPCAuditSection />
+      <ServicesAccordion />
+      <CTASection />
+      <TestimonialSection />
+      <ContactForm />
+      <PPCAudit />
+      <InsightSection />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- src/app/ads-suspension-recovery/page.js
+++ src/app/ads-suspension-recovery/page.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import Navbar from '../asample/naviagtion'
-import CookieConsent from '../cookies/cookie'
-import AdsSuspension from './home'
-import WhatSection from './what'
-import AdsProcess from './diffrent'
-import HeroSection from './proactive'
-import DetailedHeroSection from './detailed'
-import InsightSection from '../graphics/insight'
-import Footer from '../(mainpage)/footer'
+import React from "react";
+import CTASection from "../ads-audit/ctabanner";
+import Navbar from "../asample/naviagtion";
+import CookieConsent from "../cookies/cookie";
+import AdsSuspension from "./home";
+import WhatSection from "./what";
+import AdsProcess from "./diffrent";
+import HeroSection from "./proactive";
+import DetailedHeroSection from "./detailed";
+import InsightSection from "../graphics/insight";
+import Footer from "../(mainpage)/footer";
 
 const page = () => {
   return (
     <div>
-         <Navbar/>
-    <CookieConsent/>
-      <AdsSuspension/>
-      <WhatSection/>
-      <AdsProcess/>
-      <HeroSection/>
-      <DetailedHeroSection/>
-      <InsightSection/>
-      <Footer/>
+      <Navbar />
+      <CookieConsent />
+      <AdsSuspension />
+      <WhatSection />
+      <CTASection />
+      <AdsProcess />
+      <HeroSection />
+      <DetailedHeroSection />
+      <InsightSection />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
